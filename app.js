690de15914ea5f2581dd13c72e40efa21f06fe1a
Akhrorjon Sobirov <82890519+Akhrorjonuzb@users.noreply.github.com>
2022-07-09 15:49:33 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const burger=document.querySelector('#burger-menu');
+const ul=document.querySelector('nav ul');
+const nav=document.querySelector('nav');
+
+burger.addEventListener('click', ()=>{
+    ul.classList.toggle('show');
+})
+
+const navLink=document.querySelectorAll('.nav-link');
+navLink.forEach(function(e){
+    e.addEventListener('click', ()=>{
+        ul.classList.remove('show');
+    });
+});
+
+// scroll to top functionality
+const scrollUp=document.querySelector('#scroll-up');
+
+scrollUp.addEventListener('click', ()=>{
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
+
+/* function myFunction() {
+    document.getElementById("my-form").submit();
+} */
